ecda5cdeb776b7dfe9db3206f6d5158e201df596
ihatedwang <dwang040@gmail.com>
2025-05-19 12:03:58 -0400

    hide youtube playables

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Really crappy chrome extension to fix some of their BS layout design.
+Really crappy chrome extension to fix some of their BS layout design. Code is absolute trash and inefficient but I just needed something to work...
 
-* Hardcode number of videos displayed in landscape and portrait mode.
+* Change sizing of youtube thumbnails (window size / 450)
 * Auto hide youtube shorts
+* Auto hide yotube playables
+
+Issues:
+* Will break if youtube changes element name
+* Youtube will having thumbnail loading issues if you display too many videos per page
--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 (function () {
-  const pseudoIconSize = 450;
-  const removeYoutubeshort = true;
+  const PSEUDO_ICON_SIZE = 450;
+  const REMOVE_SHORTS = true;
+  const REMOVE_PLAYABLES = true;
+  const SHELF_NAME = "ytd-rich-section-renderer";
 
   function setGridItemsPerRow() {
     const gridRenderer = document.querySelector("ytd-rich-grid-renderer");
@@ -8,19 +10,33 @@
     if (gridRenderer) {
       gridRenderer.style.setProperty(
         "--ytd-rich-grid-items-per-row",
-        window.innerWidth / pseudoIconSize
+        window.innerWidth / PSEUDO_ICON_SIZE
       );
     }
   }
 
   function removeShortsShelf() {
-    const shortsShelves = document.querySelectorAll("ytd-rich-section-renderer");
+    const shortsShelves = document.querySelectorAll(SHELF_NAME);
 
     shortsShelves.forEach((shelf) => {
       const titleElement = shelf.querySelector("span#title");
       if (
         titleElement &&
-        titleElement.textContent.trim().toLowerCase() === "shorts"
+        titleElement.textContent.trim().toLowerCase().includes("shorts")
+      ) {
+        shelf.remove();
+      }
+    });
+  }
+
+  function removePlayablesShelf() {
+    const playablesShelves = document.querySelectorAll(SHELF_NAME);
+    
+    playablesShelves.forEach((shelf) => {
+      const titleElement = shelf.querySelector("span#title");
+      if (
+        titleElement &&
+        titleElement.textContent.trim().toLowerCase().includes("youtube playables")
       ) {
         shelf.remove();
       }
@@ -29,8 +45,11 @@
 
   function applyChanges() {
     setGridItemsPerRow();
-    if (removeYoutubeshort) {
+    if (REMOVE_SHORTS) {
       removeShortsShelf();
+    }
+    if (REMOVE_PLAYABLES) {
+      removePlayablesShelf();
     }
   }
 
